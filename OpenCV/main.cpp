325500baa3b46760fc57da7075b8cfe8be88282a
Aneesh <aabhansa@ucsd.eud>
2016-01-31 19:02:16 -0800

    Initial Commit

---
+++ OpenCV/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OpenCV
+//
+//  Created by Aneesh Bhansali on 1/31/16.
+//  Copyright © 2016 Aneesh Bhansali. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
