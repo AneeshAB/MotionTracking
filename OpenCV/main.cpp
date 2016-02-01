43deaecebd09c5ae89c762ab9e82dbf0244c7fea
Aneesh <aabhansa@ucsd.eud>
2016-01-31 19:24:21 -0800

    Now displays webcam feed.

--- OpenCV/main.cpp
+++ OpenCV/main.cpp
@@ -3,13 +3,37 @@
 //  OpenCV
 //
 //  Created by Aneesh Bhansali on 1/31/16.
-//  Copyright © 2016 Aneesh Bhansali. All rights reserved.
 //
 
 #include <iostream>
+#include <opencv2/opencv.hpp>
+
+using namespace cv;
+using namespace std;
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    VideoCapture webcam(0); // Webcam video feed
+    
+    // If unable to open webcam, terminate
+    if (!webcam.isOpened()) {
+        cerr << "Unable to open webcam." << endl;
+        return -1;
+    }
+    
+    // Loop until a keystroke is detected
+    while (waitKey(30) < 0) {
+        // Read in the current frame
+        Mat inFrame;
+        webcam.read(inFrame);
+        
+        // Horizontally flip the frame
+        Mat outFrame;
+        flip(inFrame, outFrame, 1);
+        
+        // Show the frame
+        imshow("Webcam", outFrame);
+    }
+    
+    // Finished without error
     return 0;
 }
